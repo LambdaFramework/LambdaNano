592027081e59c8814257d34d16176a4648c1eb85
siewyanhoh <siew.yan.hoh@cern.ch>
2020-08-03 20:31:42 +0200

    update

--- scripts/skim.py
+++ scripts/skim.py
@@ -11,6 +11,7 @@ from PhysicsTools.NanoAODTools.postprocessing.modules.analysis.WHSS.aliasProduce
 
 from multiprocessing import Process, Pool, Queue, Manager
 import multiprocessing, time
+import random
 
 class skimmer:
 
@@ -31,78 +32,22 @@ class skimmer:
             ROOT.gROOT.ProcessLine(".L %s/src/%s.cc+O" %(base,basename) )
         pass
 
-    def initialization(self):
-
-        data = [
-            'Run2016_SingleMuon',
-            'Run2016_SingleElectron',
-            'Run2016_DoubleMuon',
-            'Run2016_DoubleEG',
-            #'Run2016_MuonEG', # issue  
-            'Run2016_SingleMuon_fake',
-            'Run2016_SingleElectron_fake',
-            'Run2016_DoubleMuon_fake',
-            'Run2016_DoubleEG_fake',
-            #'Run2016_MuonEG_fake'
-        ]
-
-        mc = [
-            "DYJetsToLL_M-50-LO_ext2",
-            "DYJetsToLL_M-10to50-LO",
-            "TTTo2L2Nu",
-            "ST_s-channel",
-            "ST_t-channel_antitop",
-            "ST_t-channel_top",
-            "ST_tW_antitop",
-            "ST_tW_top",
-            "WWTo2L2Nu",
-            "WpWmJJ_EWK_noTop",
-            "GluGluWWTo2L2Nu_MCFM",
-            "Wg_MADGRAPHMLM",
-            "Zg",
-            "WZTo3LNu_mllmin01",
-            "WZTo2L2Q",
-            "ZZTo2L2Nu",
-            "ZZTo2L2Q",
-            "ZZTo4L",
-            "ZZZ",
-            "WZZ",
-            "WWZ",
-            "WWW",
-            "GluGluHToWWTo2L2NuPowheg_M125",
-            "VBFHToWWTo2L2Nu_M125",
-            "HZJ_HToWW_M125",
-            "ggZH_HToWW_M125",
-            "HWplusJ_HToWW_M125",
-            "HWminusJ_HToWW_M125",
-            "GluGluHToTauTau_M125",
-            "VBFHToTauTau_M125",
-            "HZJ_HToTauTau_M125",
-            "HWplusJ_HToTauTau_M125",
-            "HWminusJ_HToTauTau_M125"
-        ]
+    def initialization(self, year_):
+        if not os.environ['NANOAODTOOLS_BASE']:
+            print("ERROR: NANOAODTOOLS_BASE not set"); sys.exit();
+            
+        theList = map( lambda x: x.split('.')[0] , os.listdir( '%s/scripts/filelists/%s' %( os.environ["NANOAODTOOLS_BASE"] , year_ ) ) )
+        data = [ x for x in theList if any(y in x for y in [ 'Single' ,'Double' , 'EG' ]) ]
+        mc = list(set(theList)^set(data))
         
-        #dummy = [ "ZZZ" , "WZZ" , "WWW" , "ZZTo2L2Q" , "ST_s-channel" ]
-        dummy = [
-            'Run2016_MuonEG',
-            'Run2016_MuonEG_fake'
-        ]
-        #dummy = [ "TTTo2L2Nu" , "ZZTo4L" ]
-        #dummy = [ "TTTo2L2Nu" ]
+        dummy = [ "ZZZ" , "WZZ" , "WWW" , "ZZTo2L2Q" , "ST_s-channel" ]
 
-        #data module
-        dataMod = [ bVetoer() , alias() ]
-        
-        if self.dataset_ == 0 :
-            fnames = data + mc # no, dont run this
-            self.modules = dataMod
-        elif self.dataset_ == 1 :
-            fnames = mc
-        elif self.dataset_ == 2 :
-            fnames = data ; self.modules = dataMod
-        else :
-            if any('Run' in x for x in dummy) : self.modules = dataMod
-            fnames = dummy # testing
+        if   self.dataset_ == 0 : fnames = data + mc ; # no, dont run this
+        elif self.dataset_ == 1 : fnames = mc ;
+        elif self.dataset_ == 2 : fnames = data ;
+        else : fnames = dummy # testing
+
+        if self.dataset_ == 2 : self.modules = [ bVetoer() , alias() ]
 
         # load libraries only once
         if self.dataset_ == 1 or self.dataset_ > 2 :
@@ -111,16 +56,13 @@ class skimmer:
         
         print("Running on : " if self.dataset_ < 3 else "Running on dummy : ", fnames)
         
-        for i in fnames:
-            sample_files = open( "%s/scripts/filelists/%s.txt" %(os.getcwd(),i) , "r" )
+        for i in fnames :
+            sample_files = open( "%s/scripts/filelists/%s/%s.txt" %( os.environ["NANOAODTOOLS_BASE"] , year_ , i ) , "r" )
             self.samples[i] = [ x.strip('\n') for x in sample_files.readlines() ]
             sample_files.close()
         pass
     
     def run( self , infiles , supercuts ):
-
-        # data species
-        isMC = False if 'Run' in infiles[0].split('/')[-1] else True
         
         p=PostProcessor(
             outputDir='%s/%s/%s/' %( os.getcwd() , self.outfolder_ , isample ) ,
@@ -186,6 +128,7 @@ if __name__ == "__main__" :
 
     usage = "usage: %prog [options]"
     parser.add_option("-d", "--dataset", action="store", type="int", dest="dataset", default=None , help="specify which dataset to run, 0 : all ; 1 : mc ; 2 : data ; >3 : dummy" )
+    parser.add_option("-y", "--year", action="store", type="string", dest="year", default="2016" , help="data-taking year [default: 2016]")
     parser.add_option("-o", "--outfolder", action="store", type="string", dest="outfolder", default="skimmed" , help="name of the output folder [default: skimmed]")
     (options, args) = parser.parse_args()
 
@@ -202,9 +145,11 @@ if __name__ == "__main__" :
     start_time = time.time()
 
     ############################################################################
+    # Preselection
     presel="mll>12 && Lepton_pt[0]>25 && Lepton_pt[1]>20 && PuppiMET_pt > 30"
-    skim = skimmer( options.dataset , options.outfolder )
-    skim.initialization()
+    
+    skim = skimmer( options.dataset , '%s-%s' %(options.outfolder,options.year) )
+    skim.initialization(options.year)
     
     print "Number of cpu : ", multiprocessing.cpu_count()
 
@@ -252,7 +197,7 @@ if __name__ == "__main__" :
         for isample in skim.samples:
             filelist = skim.samples[isample]
             for ifile in filelist:
-                if 'MuonEG_Run2016B' not in ifile: continue
+                #if 'MuonEG_Run2016B' not in ifile: continue
                 proc = Process(target=skim.run, args=([ifile],presel,))
                 procs.append(proc) ; proc.start()
             [ r.join() for r in procs  ]
