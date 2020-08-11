ee09d351bffab50fe7b2fc721c7e095a5e5a728f
siewyanhoh <siew.yan.hoh@cern.ch>
2020-08-11 18:00:20 +0200

    added serialism

--- scripts/latino_postproc.py
+++ scripts/latino_postproc.py
@@ -5,8 +5,8 @@ ROOT.PyConfig.IgnoreCommandLineOptions = True
 from importlib import import_module
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
 from PhysicsTools.NanoAODTools.postprocessing.modules.analysis.WHSS.bVetoProducer import bVetoer
-from PhysicsTools.NanoAODTools.postprocessing.modules.analysis.WHSS.lepSFProducerCpp import lepSF
-from PhysicsTools.NanoAODTools.postprocessing.modules.analysis.WHSS.pujetIdSFProducerCpp import pujetIdSF
+from PhysicsTools.NanoAODTools.postprocessing.modules.analysis.WHSS.lepSFProducerCpp import lepSFProducerCpp #lepSF
+from PhysicsTools.NanoAODTools.postprocessing.modules.analysis.WHSS.pujetIdSFProducerCpp import pujetIdSFProducerCpp #pujetIdSF
 from PhysicsTools.NanoAODTools.postprocessing.modules.analysis.WHSS.aliasProducer import alias
 
 from multiprocessing import Process, Pool, Queue, Manager
@@ -15,11 +15,12 @@ import random
 
 class skimmer:
 
-    def __init__(self, dataset_ , outfolder_ ):
+    def __init__(self, dataset_ , outfolder_ , year_ ):
         self.dataset_ = dataset_
         self.outfolder_ = outfolder_
+        self.year_ = year_
         self.samples={}
-        self.modules = [ lepSF() , pujetIdSF() , bVetoer() , alias() ]
+        self.modules = []
 
         if not os.path.isdir('%s/%s' %( os.getcwd() , self.outfolder_ ) ): os.mkdir('%s/%s' %( os.getcwd() , self.outfolder_ ) )
         
@@ -32,21 +33,26 @@ class skimmer:
             ROOT.gROOT.ProcessLine(".L %s/src/%s.cc+O" %(base,basename) )
         pass
 
-    def initialization(self, year_):
+    def initialization(self):
         if not os.environ['NANOAODTOOLS_BASE']:
             print("ERROR: NANOAODTOOLS_BASE not set"); sys.exit();
             
-        theList = map( lambda x: x.split('.')[0] , os.listdir( '%s/scripts/filelists/%s' %( os.environ["NANOAODTOOLS_BASE"] , year_ ) ) )
+        theList = map( lambda x: x.split('.')[0] , os.listdir( '%s/scripts/filelists/%s' %( os.environ["NANOAODTOOLS_BASE"] , self.year_ ) ) )
         data = [ x for x in theList if any(y in x for y in [ 'Single' ,'Double' , 'EG' ]) ]
         mc = list(set(theList)^set(data))
         
-        dummy = [ "ZZZ" , "WZZ" , "WWW" , "ZZTo2L2Q" , "ST_s-channel" ]
+        dummy = [ "ZZZ" ] #, "WZZ" , "WWW" , "ZZTo2L2Q" , "ST_s-channel" ]
 
         if   self.dataset_ == 0 : fnames = data + mc ; # no, dont run this
         elif self.dataset_ == 1 : fnames = mc ;
         elif self.dataset_ == 2 : fnames = data ;
         else : fnames = dummy # testing
 
+        ###### modules ############
+        lepSF = lambda : lepSFProducerCpp( self.year_ , 2 , 'total_SF' )
+        pujetIdSF = lambda : pujetIdSFProducerCpp( self.year_ , 'loose' )
+
+        if self.dataset_ == 1 or self.dataset_ > 2 : self.modules = [ lepSF() , pujetIdSF() , bVetoer() , alias() ]
         if self.dataset_ == 2 : self.modules = [ bVetoer() , alias() ]
 
         # load libraries only once
@@ -57,7 +63,7 @@ class skimmer:
         print("Running on : " if self.dataset_ < 3 else "Running on dummy : ", fnames)
         
         for i in fnames :
-            sample_files = open( "%s/scripts/filelists/%s/%s.txt" %( os.environ["NANOAODTOOLS_BASE"] , year_ , i ) , "r" )
+            sample_files = open( "%s/scripts/filelists/%s/%s.txt" %( os.environ["NANOAODTOOLS_BASE"] , self.year_ , i ) , "r" )
             self.samples[i] = [ x.strip('\n') for x in sample_files.readlines() ]
             sample_files.close()
         pass
@@ -68,7 +74,7 @@ class skimmer:
             outputDir='%s/%s/%s/' %( os.getcwd() , self.outfolder_ , isample ) ,
             inputFiles=infiles ,
             cut=supercuts,
-            branchsel= "%s/scripts/data/slimming-2016-in.txt" %os.getcwd(),
+            branchsel= "%s/scripts/data/slimming-%s-in.txt" %( os.getcwd() , self.year_ ),
             modules= self.modules ,
             compression="LZMA:9",
             friend=False,
@@ -81,7 +87,7 @@ class skimmer:
             fwkJobReport=False,
             histFileName=None,
             histDirName=None,
-            outputbranchsel = "%s/scripts/data/slimming-2016-in.txt" %os.getcwd(),
+            outputbranchsel = "%s/scripts/data/slimming-%s-in.txt" %( os.getcwd() , self.year_ ),
             maxEntries=None,
             firstEntry=0,
             prefetch=False,
@@ -130,6 +136,8 @@ if __name__ == "__main__" :
     parser.add_option("-d", "--dataset", action="store", type="int", dest="dataset", default=None , help="specify which dataset to run, 0 : all ; 1 : mc ; 2 : data ; >3 : dummy" )
     parser.add_option("-y", "--year", action="store", type="string", dest="year", default="2016" , help="data-taking year [default: 2016]")
     parser.add_option("-o", "--outfolder", action="store", type="string", dest="outfolder", default="skimmed" , help="name of the output folder [default: skimmed]")
+    parser.add_option("-s", "--serialism", action="store_true", dest="serialism", default=False , help="toggle parallelism/serialism in python, useful for testing [default: False]")
+
     (options, args) = parser.parse_args()
 
     options.outfolder = options.outfolder if options.dataset < 3 else "dummy"
@@ -148,15 +156,15 @@ if __name__ == "__main__" :
     # Preselection
     presel="mll>12 && Lepton_pt[0]>25 && Lepton_pt[1]>20 && PuppiMET_pt > 30"
     
-    skim = skimmer( options.dataset , '%s-%s' %(options.outfolder,options.year) )
-    skim.initialization(options.year)
+    skim = skimmer( options.dataset , '%s-%s' %(options.outfolder,options.year) , options.year )
+    skim.initialization()
     
     print "Number of cpu : ", multiprocessing.cpu_count()
 
     # number of task/process
     nTask = 30
 
-    # Parallelism
+    # Parallelism (in production mode, parallelism always on)
     if options.dataset == 1 : # mc
         
         procs = []
@@ -193,23 +201,24 @@ if __name__ == "__main__" :
             parallalizedByFiles( filelist , presel, q , skim.run , round(float(len(filelist))/nTask) )
             
     else:
-        procs = []
-        for isample in skim.samples:
-            filelist = skim.samples[isample]
-            for ifile in filelist:
-                #if 'MuonEG_Run2016B' not in ifile: continue
-                proc = Process(target=skim.run, args=([ifile],presel,))
-                procs.append(proc) ; proc.start()
+
+        if not options.serialism:
+            print " --> DUMMY : Running with parallelism"
+            procs = []
+            for isample in skim.samples:
+                filelist = skim.samples[isample]
+                for ifile in filelist:
+                    #if 'MuonEG_Run2016B' not in ifile: continue
+                    proc = Process(target=skim.run, args=([ifile],presel,))
+                    procs.append(proc) ; proc.start()
             [ r.join() for r in procs  ]
+        else:
+            print " --> DUMMY : Running with serialism"
+            for isample in skim.samples:
+                filelist = skim.samples[isample]
+                skim.run( filelist , presel )
 
-        #procs = []
-        #m = Manager() ; q = m.Queue() ; p = Pool(12)
-        #for isample in skim.samples:
-        #    print("isample : ", isample)
-        #    filelist = skim.samples[isample]
-        #    parallalizedByFiles( filelist , presel, q , skim.run , round(float(len(filelist))/nTask) )
-
-    
+    ## benchmarking proram ##
     print("--- %s seconds ---" % (time.time() - start_time))
     print("--- %s minutes ---" % ( (time.time() - start_time)/60. ))
     print("--- %s hours ---" % ( (time.time() - start_time)/3600. ))
