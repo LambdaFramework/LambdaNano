20fe7ca62ab02f33b3776396ee6cb09bb2da5367
siewyanhoh <siew.yan.hoh@cern.ch>
2020-08-11 17:59:20 +0200

    top up missing 2017 datasets and adding slimming.txt for 17/18

--- scripts/filelists/createFilelist.py
+++ scripts/filelists/createFilelist.py
@@ -3,14 +3,20 @@
 import os
 
 # 2016
-MCdir="/media/shoh/02A1ACF427292FC0/nanov5/Summer16_102X_nAODv5_Full2016v6/MCl1loose2016v6__MCCorr2016v6__l2loose__l2tightOR2016v6"
-DATAdir="/media/shoh/02A1ACF427292FC0/nanov5/Run2016_102X_nAODv5_Full2016v6/DATAl1loose2016v6__l2loose__l2tightOR2016v6"
-FAKEdir="/media/shoh/02A1ACF427292FC0/nanov5/Run2016_102X_nAODv5_Full2016v6_ForNewWPs/DATAl1loose2016v6__l2loose__fakeW"
+#MCdir="/media/shoh/02A1ACF427292FC0/nanov5/Summer16_102X_nAODv5_Full2016v6/MCl1loose2016v6__MCCorr2016v6__l2loose__l2tightOR2016v6"
+#DATAdir="/media/shoh/02A1ACF427292FC0/nanov5/Run2016_102X_nAODv5_Full2016v6/DATAl1loose2016v6__l2loose__l2tightOR2016v6"
+#FAKEdir="/media/shoh/02A1ACF427292FC0/nanov5/Run2016_102X_nAODv5_Full2016v6_ForNewWPs/DATAl1loose2016v6__l2loose__fakeW"
 
 # 2017
-#MCdir="/media/shoh/02A1ACF427292FC0/nanov5/Fall2017_102X_nAODv5_Full2017v6/MCl1loose2017v6__MCCorr2017v6__l2loose__l2tightOR2017v6"
-#DATAdir="/media/shoh/02A1ACF427292FC0/nanov5/Run2017_102X_nAODv5_Full2017v6/DATAl1loose2017v6__l2loose__l2tightOR2017v6"
-#FAKEdir="/media/shoh/02A1ACF427292FC0/nanov5/Run2017_102X_nAODv5_Full2017v6_ForNewWPs/DATAl1loose2017v6__l2loose__fakeW"
+MCdir="/media/shoh/02A1ACF427292FC0/nanov5/Fall2017_102X_nAODv5_Full2017v6/MCl1loose2017v6__MCCorr2017v6__l2loose__l2tightOR2017v6"
+DATAdir="/media/shoh/02A1ACF427292FC0/nanov5/Run2017_102X_nAODv5_Full2017v6/DATAl1loose2017v6__l2loose__l2tightOR2017v6"
+FAKEdir="/media/shoh/02A1ACF427292FC0/nanov5/Run2017_102X_nAODv5_Full2017v6_ForNewWPs/DATAl1loose2017v6__l2loose__fakeW"
+
+# 2018
+#MCdir="/media/shoh/02A1ACF427292FC0/nanov5/Autumn18_102X_nAODv6_Full2018v6/MCl1loose2018v6__MCCorr2018v6__l2loose__l2tightOR2018v6"
+#DATAdir="/media/shoh/02A1ACF427292FC0/nanov5/Run2018_102X_nAODv6_Full2018v6/DATAl1loose2018v6__l2loose__l2tightOR2018v6"
+#FAKEdir="/media/shoh/02A1ACF427292FC0/nanov5/Run2018_102X_nAODv6_Full2018v6_ForNewWPs/DATAl1loose2018v6__l2loose__fakeW"
+
 
 year="2016"
 if "2017" in MCdir and "2017" in DATAdir and "2017" in FAKEdir: year = "2017"
